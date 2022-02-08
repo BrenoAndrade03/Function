b448898a060f9b490a11e1f1feb14e1d40040259
BrenoAndrade03 <brenopereira263@gmail.com>
2022-02-08 11:06:08 -0300

    funões 1.0

---
+++ script.js
@@ -0,0 +1,23 @@
+//Tópicos 1,2 e 3:
+function características (nome,idade, estilo_de_musica){
+    console.log(`Eaí? Meu nome é ${nome}, eu tenho ${idade} anos. E eu gosto muito de musicas do tipo ${estilo_de_musica}.`);
+}
+//tópico 4:
+function preferidos(filme, musica){
+    console.log(`O filme que eu mais gosto é ${filme} ele é sensacional! E eu também gosto muito da música ${musica}.`);
+}
+//bonus:
+function bonus(X){
+    return X *3;
+}
+
+
+
+
+
+
+
+
+características("Breno", "18", "Pop");
+preferidos("Encanto", "Tell me milet");
+console.log(bonus(7));
